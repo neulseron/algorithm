1ae5942a5738603bca485053def98f906f420696
Suram <tnfka905@naver.com>
2020-04-16 23:36:18 +0900

    2020.04.16 문제풀이

---
+++ 최수람/2020.04.16/Solution_2579_계단오르기.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Solution_2579_계단오르기 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N;
+		int stair[],dp[];
+		N=sc.nextInt();
+		stair=new int [N+1];
+		dp=new int [N+1];
+		for(int i=1;i<=N;i++){
+			stair[i]=sc.nextInt();
+		}
+		dp[1]=stair[1];
+		if(N>1)
+			dp[2]=Math.max(stair[1], stair[1]+stair[2]);
+		for(int i=3;i<=N;i++){
+			dp[i]=Math.max(dp[i-2]+stair[i],dp[i-3]+stair[i-1]+stair[i]);
+		}
+		System.out.println(dp[N]);
+	}
+}
