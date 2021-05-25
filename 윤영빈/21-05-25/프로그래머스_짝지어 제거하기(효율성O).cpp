7d77c08b1be7909e2a4809931540d6ce3b1170b0
neulsesron <new5064@naver.com>
2021-05-25 19:51:43 +0900

    Create 프로그래머스_짝지어 제거하기(효율성O).cpp
    스택

---
+++ 윤영빈/21-05-25/프로그래머스_짝지어 제거하기(효율성O).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> str;
+    
+    str.push(s[0]);
+    
+    for (int i = 1; i < s.length(); i++) {
+        if (!str.empty() && str.top() == s[i]) {
+            str.pop();
+        } else {
+            str.push(s[i]);
+        }
+    }
+    
+    if (str.empty())
+        return 1;
+
+    return 0;
+}
