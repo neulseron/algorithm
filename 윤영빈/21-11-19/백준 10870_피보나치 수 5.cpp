09feefee37d24baf048b31e1fcc9be1089ccaa6a
neulsesron <new5064@naver.com>
2021-11-19 17:38:26 +0900

    Create 백준 10870_피보나치 수 5.cpp

---
+++ 윤영빈/21-11-19/백준 10870_피보나치 수 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> fibo (N+1, 0);
+    fibo[1] = 1;
+
+    for (int i = 2; i <= N; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+
+    cout << fibo[N];
+}
