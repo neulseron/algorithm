afd42df7f57f846c0ac2af0da7d932c4bd64d4d0
neulsesron <new5064@naver.com>
2021-12-09 17:02:57 +0900

    Create 해커랭크_Print Pretty.cpp

---
+++ 윤영빈/21-12-09/해커랭크_Print Pretty.cpp
@@ -0,0 +1,6 @@
+        cout << setw(0) << hex << nouppercase << showbase << (long)A << "\n";
+
+        cout << setfill('_') << setw(15) << fixed << right << showpos << setprecision(2) << B << "\n";
+        
+        cout << resetiosflags(ios::showpos);
+        cout << setw(15) << uppercase << scientific << setprecision(9) << C << "\n";
